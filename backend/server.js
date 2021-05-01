454db94386fa2a22e0eb63dbed523435295d1ae9
justinHe123 <justinhe@ucla.edu>
2021-04-30 20:07:14 -0700

    updated configuration code

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,32 @@
-// const express = require('express')
-
-// // https://developers.google.com/recaptcha/docs/verify
-// // https://www.google.com/recaptcha/api/siteverify
-
-// const app = express();
-
-// app.set('etag', false)
-
 // app.get('/', (req, res) => res.render('./test'))
 const express = require('express')
+const { Emails } = require("./tables.js")
+
 const app = express()
 app.set('etag', false)
 const PORT = process.env.PORT || 5000
 
 // Setting up the body parser - will move if this should go elsewhere
 const bodyParser = require('body-parser')
-app.use(bodyParser.text())
+app.use(bodyParser.urlencoded())
 
-app.post('/email/submit',
-        (req, res) => {
+// Email endpoint
+app
+  .post('/email/submit', submitEmail)
+
+// Temp place for submit email function
+const submitEmail = (req, res) => {
   // TODO: check for a valid email
   const re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
   if(re.test(email)) {
     Emails.create({email: req.body.email})
-    res.sendStatus(201)
+    return res.sendStatus(201)
   } else {
-    res.sendStatus(400)
+    return res.sendStatus(400)
   }
-})
+}
 
 // Filler endpoints
-app
-  .get('/', (req, res) => res.sendFile('test.html', {root: __dirname}))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => res.sendFile('test.html', {root: __dirname}))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
