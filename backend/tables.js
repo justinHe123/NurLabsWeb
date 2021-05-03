b9dcb892a7530c41f60126cc787c32329c433876
Vivian Ha <vivian.ha534@gmail.com>
2021-05-02 19:50:14 -0700

    set up postgres w/ environmental variables

--- backend/tables.js
+++ backend/tables.js
@@ -1,16 +1,39 @@
-const { Sequelize, DataTypes } = require('sequelize')
+const { Sequelize, DataTypes } = require("sequelize");
+require('dotenv').config()
 
 // TODO: config
-const sql = new Sequelize()
-
-const Emails = sequelize.define('emails', {
-    email: {
-        type: DataTypes.STRING,
-        primaryKey: true
+const sequelize = new Sequelize(
+  `${process.env.DB}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        // Heroku requires us to use SSL. https://devcenter.heroku.com/changelog-items/2035
+        require: true,
+        // Unless we have an authorized SSL Cert to work with, we'll be rejected & get an error
+        // Thus, we need to pass in rejectUnauthorized: false to work around this error
+        rejectUnauthorized: false,
+      },
     },
-    // verified: {
-    //     type: DataTypes.BOOLEAN,
-    //     defaultValue: false
-    // }
-})
+  }
+);
 
+
+const testConnection = async () => {
+  console.log("start Authentication");
+  await sequelize.authenticate();
+  console.log("Successfully Connected");
+};
+
+// testConnection()
+
+const Emails = sequelize.define("emails", {
+  email: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+  },
+  // verified: {
+  //     type: DataTypes.BOOLEAN,
+  //     defaultValue: false
+  // }
+});
