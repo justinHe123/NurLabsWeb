c2e153a42808eac6b594c95a1367711e319bb64b
justinHe123 <justinhe@ucla.edu>
2021-05-07 10:10:48 -0700

    test db changes

--- backend/tables.js
+++ backend/tables.js
@@ -1,7 +1,8 @@
 const { Sequelize, DataTypes } = require("sequelize");
 require('dotenv').config()
 
-const dburl = `${process.env.DB}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+// const dburl = `${process.env.DB}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+const dburl = process.env.DATABASE_URL;
 console.log(dburl)
 
 const sequelize = new Sequelize(
