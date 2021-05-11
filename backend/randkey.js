409136dc64a152b050df3138a6102550852ca704
Darren <darrenzhang22@gmail.com>
2021-05-11 11:09:00 -0700

    Create function that generates psuedo-random keys for an email

---
+++ backend/randkey.js
@@ -0,0 +1,25 @@
+const { SHA3 } = require('sha3');
+
+const randChars = "qwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*())-_+={}|[]\\`~<>?,./;:\'\"";
+const n = randChars.length;
+
+const generateRandKey = (email) => {
+  const timeInHex = (Date.now()).toString(16);
+  let input = email +timeInHex;
+
+  //add random characters to the front and end of string
+  const randLen = Math.floor(Math.random() * 7)+11;
+  for (let i = 0; i < randLen; i++) {
+    input += randChars[Math.floor(Math.random()*n)];
+  }
+  const randLen2 = Math.floor(Math.random() * 23)+3;
+  for (let i = 0; i < randLen2; i++) {
+    input = randChars[Math.floor(Math.random()*n)] + input;
+  }
+
+  //generate hash
+  const hash = new SHA3(512);
+  hash.update(input);
+  const key = hash.digest('hex');
+  return key;
+}
