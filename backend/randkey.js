235e047b60475a461c40b9bc0f7425a921adb759
Darren <darrenzhang22@gmail.com>
2021-05-12 19:56:10 -0700

    Replace sha3 with uuid for random key generation

--- backend/randkey.js
+++ backend/randkey.js
@@ -1,27 +1,7 @@
-const { SHA3 } = require('sha3');
+const { v4: uuidv4 } = require('uuid');
 
-const randChars = "qwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*())-_+={}|[]\\`~<>?,./;:\'\"";
-const n = randChars.length;
-
-const generateRandKey = (email) => {
-  const timeInHex = (Date.now()).toString(16);
-  let input = email +timeInHex;
-
-  //add random characters to the front and end of string
-  const randLen = Math.floor(Math.random() * 7)+11;
-  for (let i = 0; i < randLen; i++) {
-    input += randChars[Math.floor(Math.random()*n)];
-  }
-  const randLen2 = Math.floor(Math.random() * 23)+3;
-  for (let i = 0; i < randLen2; i++) {
-    input = randChars[Math.floor(Math.random()*n)] + input;
-  }
-
-  //generate hash
-  const hash = new SHA3(512);
-  hash.update(input);
-  const key = hash.digest('hex');
-  return key;
+const generateRandKey = () => {
+  return uuidv4();
 }
 
 module.exports = generateRandKey;
