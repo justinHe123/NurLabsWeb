7bec46f44da85b2b7b9d2930312d6ecf3bfd5b1a
Trevor <tr89on@gmail.com>
2021-05-16 23:11:12 -0700

    restore stuff that got randomly deleted

--- backend/server.js
+++ backend/server.js
@@ -42,15 +42,38 @@ const submitContact = (req, res) => {
 
 const submitEmail = async (req, res) => {
   try {
-    if (validEmail(req.body.email)) {
-      await Emails.create({email: req.body.email});
-      return res.sendStatus(201);
-    } else {
-      return res.sendStatus(400);
-    }
+    if(!validEmail(req.body.email)) return res.sendStatus(400)
+    await Emails.create({email: req.body.email})
+    return res.sendStatus(201)
   } 
   catch (error) {
-    return res.sendStatus(500);
+    return res.sendStatus(500)
+  }
+}
+
+const unsubscribeEmail = async (req, res) => {
+  try {
+    // TODO: Instead of doing this, use schema validation
+    if (!req.body.uuid || !req.body.email) return res.sendStatus(400)
+    const email = await Emails.findByPk(req.body.uuid)
+    if (email === null || email.email !== req.body.uuid) return res.sendStatus(404)
+    await email.destroy()
+    return res.sendStatus(200)
+  } 
+  catch (error) {
+    return res.sendStatus(500)
+  }
+}
+
+const checkEmail = async (req,res) => {
+  try{
+    if (!req.query.uuid || !req.query.email) return res.sendStatus(400)
+    const email = await Emails.findByPk(req.query.uuid)
+    if (email === null || email.email !== req.query.uuid) return res.sendStatus(404)
+    return res.sendStatus(200)
+  } 
+  catch (error) {
+    return res.sendStatus(500)
   }
 }
 
@@ -67,18 +90,16 @@ const getEmails = async (req, res) => {
 //////////////// ENDPOINTS ////////////////
 
 // Contact endpoint
-// app
-//   .post('/contact/submit', verifyRecaptcha, submitContact)
 app
-  .post('/contact/submit', submitContact);
+  .post('/contact/submit', verifyRecaptcha, submitContact)
 
 // Email endpoints
 app
   .post('/email/submit', verifyRecaptcha, submitEmail);
-// app
-//   .post('/email/unsubscribe', /* verifyRecaptcha, */ unsubscribeEmail)
-// app
-//   .get('/email/check', checkEmail)
+app
+  .post('/email/unsubscribe', /* verifyRecaptcha, */ unsubscribeEmail)
+app
+  .get('/email/check', checkEmail)
 
 // Filler endpoints
 app
--- backend/tables.js
+++ backend/tables.js
@@ -1,47 +1,58 @@
-// const { Sequelize, DataTypes } = require("sequelize");
-// require('dotenv').config()
+const { Sequelize, DataTypes } = require("sequelize");
+require('dotenv').config()
 
-// // const dburl = `${process.env.DB}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-// const dburl = process.env.DB_URL;
-
-// const sequelize = new Sequelize(
-//   dburl,
-//   {
-//     dialect: 'postgres',
-//     dialectOptions: {
-//       ssl: {
-//         // Heroku requires us to use SSL. https://devcenter.heroku.com/changelog-items/2035
-//         require: true,
-//         // Unless we have an authorized SSL Cert to work with, we'll be rejected & get an error
-//         // Thus, we need to pass in rejectUnauthorized: false to work around this error
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   }
-// );
+let dburl;
+if (process.env.DATABASE_URL) dburl = process.env.DATABASE_URL;
+else dburl = `${process.env.DB}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 
-// const testConnection = async () => {
-//   console.log("start Authentication");
-//   await sequelize.authenticate();
-//   console.log("Successfully Connected");
-// };
+const sequelize = new Sequelize(
+  dburl,
+  {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        // Heroku requires us to use SSL. https://devcenter.heroku.com/changelog-items/2035
+        require: true,
+        // Unless we have an authorized SSL Cert to work with, we'll be rejected & get an error
+        // Thus, we need to pass in rejectUnauthorized: false to work around this error
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 
-// testConnection()
 
-// const Emails = sequelize.define("Emails", {
-//   email: {
-//     type: DataTypes.STRING,
-//     primaryKey: true,
-//   },
-//   // verified: {
-//   //     type: DataTypes.BOOLEAN,
-//   //     defaultValue: false
-//   // }
-// });
+const testConnection = async () => {
+  console.log("start Authentication");
+  await sequelize.authenticate();
+  console.log("Successfully Connected");
+};
 
-// Emails.sync()
+testConnection()
 
-// module.exports = {
-//     Emails
-// }
+const Emails = sequelize.define("Emails", {
+  uuid: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true
+  },
+  email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+  }},
+  {
+    indexes: [{
+      unique: true,
+      fields: ['email']
+    }]
+  }
+);
+
+Emails.sync()
+
+module.exports = {
+    Emails
+}
